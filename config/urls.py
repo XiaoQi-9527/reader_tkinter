e396e768deb513de9074d03ecb32bc2fad802970
Aiden <2064501827@qq.com>
2021-03-30 13:29:56 +0800

    书源更新

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 # 链接
-urls = {"笔趣阁": ["http://www.loubiqu.com/", "http://www.loubiqu.com/modules/article/search.php?searchkey={0}"],
+urls = {"笔趣阁": ["https://www.biquge.info/", "https://www.biquge.info/modules/article/search.php?searchkey={0}"],
         "起点": ["https://www.qidian.com/", "https://www.qidian.com/search?kw={0}"],
         "代理": ["http://www.66ip.cn/", "https://www.kuaidaili.com/free/inha/", "http://www.goubanjia.com/"]}
 
